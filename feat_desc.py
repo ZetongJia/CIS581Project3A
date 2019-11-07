401320b204174fdc3d63c11eea06c12be6085631
ZetongJia <zetongj@seas.upenn.edu>
2019-11-06 20:09:30 -0500

    Add files via upload

---
+++ feat_desc.py
@@ -0,0 +1,47 @@
+'''
+  File name: feat_desc.py
+  Author:
+  Date created:
+'''
+
+'''
+  File clarification:
+    Extracting Feature Descriptor for each feature point. You should use the subsampled image around each point feature, 
+    just extract axis-aligned 8x8 patches. Note that it’s extremely important to sample these patches from the larger 40x40 
+    window to have a nice big blurred descriptor. 
+    - Input img: H × W matrix representing the gray scale input image.
+    - Input x: N × 1 vector representing the column coordinates of corners.
+    - Input y: N × 1 vector representing the row coordinates of corners.
+    - Outpuy descs: 64 × N matrix, with column i being the 64 dimensional descriptor (8 × 8 grid linearized) computed at location (xi , yi) in img.
+'''
+import numpy as np
+from skimage import filters
+from sklearn import preprocessing
+from interp import interp2
+
+def feat_desc(img, x, y):
+  [N] = x.shape
+  descs = np.zeros((64, N))
+  edges = filters.sobel(img)
+  
+  for i in range(N):
+    y_temp = np.arange(y[i]-19, y[i]+21, 1)
+    y_i = np.tile(y_temp,(40,1))
+    x_temp = np.arange(x[i]-19, x[i]+21, 1)
+    x_temp = x_temp.reshape(-1,1)
+    x_i = np.tile(x_temp,(1,40))
+    interp = interp2(edges, x_i, y_i)
+    final = np.zeros((320,5))
+    final[0:40,0:5] = interp[0:40, 0:5]
+    final[40:80,0:5] = interp[0:40, 5:10]
+    final[80:120,0:5] = interp[0:40, 10:15]
+    final[120:160,0:5] = interp[0:40, 15:20]
+    final[160:200,0:5] = interp[0:40, 20:25]
+    final[200:240,0:5] = interp[0:40, 25:30]
+    final[240:280,0:5] = interp[0:40, 30:35]
+    final[280:320,0:5] = interp[0:40, 35:40]
+    final = final.reshape((64,25))
+    final_max = np.amax(final, axis=1)
+    descs_i = preprocessing.scale(final_max)
+    descs[0:64,i:i+1] = descs_i.reshape((64,1))
+  return descs
---
+++ feat_match.py
@@ -0,0 +1,28 @@
+'''
+  File name: feat_match.py
+  Author:
+  Date created:
+'''
+
+'''
+  File clarification:
+    Matching feature descriptors between two images. You can use k-d tree to find the k nearest neighbour. 
+    Remember to filter the correspondences using the ratio of the best and second-best match SSD. You can set the threshold to 0.6.
+    - Input descs1: 64 × N1 matrix representing the corner descriptors of first image.
+    - Input descs2: 64 × N2 matrix representing the corner descriptors of second image.
+    - Outpuy match: N1 × 1 vector where match i points to the index of the descriptor in descs2 that matches with the
+                    feature i in descriptor descs1. If no match is found, you should put match i = −1.
+'''
+from pyflann import *
+import numpy as np
+
+def feat_match(descs1, descs2):
+  [c,N1] = descs1.shape
+  flann = FLANN()
+  result, dists = flann.nn(
+      np.transpose(descs2), np.transpose(descs1), 2, algorithm="kmeans", branching=32, iterations=7, checks=16)
+  match = np.zeros((N1, 1))
+  match = result[:,0]
+  match[dists[:,0]/dists[:,1] > 0.6] = -1
+
+  return match
