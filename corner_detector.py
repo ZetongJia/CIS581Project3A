5ad9948f346f9fc6b1f090050e4ed2354881db74
Rachel (Qiao) Han <hanqiao69@gmail.com>
2019-11-09 11:20:23 -0500

    Update corner_detector.py

--- corner_detector.py
+++ corner_detector.py
@@ -12,6 +12,22 @@
     - Output cimg: H × W matrix representing the corner metric matrix.
 '''
 
+import skimage.feature
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import imageio
+
+
 def corner_detector(img):
-  # Your Code Here
+
+  img_gray=(0.3*img[:,:,0]+0.59*img[:,:,1]+0.11*img[:,:,2]).astype(np.uint8)
+  cimg=skimage.feature.corner_harris(img_gray)
   return cimg
+
+# I = np.array(Image.open('left.jpg').convert('RGB'))
+# cimg = corner_detector(I)
+# print(cimg)
+# plt.imshow(I)
+# plt.show()
