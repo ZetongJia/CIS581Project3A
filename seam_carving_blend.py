5339d4e7495d59de12bb72c2d36877d528639e76
Qiao Han <hanqiao69@gmail.com>
2019-11-11 23:28:40 -0500

    make seam carving stitch RGB instead of greyscale

--- seam_carving_blend.py
+++ seam_carving_blend.py
@@ -10,7 +10,7 @@ def rgb2gray(I_rgb):
   return I_gray
 
 def getVerSeam(I, Mx, Tbx):
-  [x, y] = I.shape
+  [x, y, _] = I.shape
   E = Mx.min(1)[x-1]
 
   idxs = np.zeros((x), dtype='int')
@@ -23,18 +23,23 @@ def getVerSeam(I, Mx, Tbx):
   return idxs
 
 def get_overlapping_mask(imgA, imgB):
-	maskA = np.where(imgA>0, imgA, 0)
-	maskB = np.where(imgB>0, imgB, 0)
+	# maskA = np.where(imgA>0, imgA, 0)
+	# maskB = np.where(imgB>0, imgB, 0)
+	maskA = np.logical_or(imgA[:,:,0]>0,imgA[:,:,1]>0,imgA[:,:,2]>0)
+	maskB = np.logical_or(imgB[:,:,0]>0,imgB[:,:,1]>0,imgB[:,:,2]>0)
 	maskAB = np.logical_and(maskA>0,maskB>0)
 	return maskAB
 
 def seam_carving_blend_left(imgA, imgB):
-	maskAB = get_overlapping_mask(imgA, imgB)
-	top = min(np.argwhere(maskAB>0), key=lambda x: x[0])[0]
-	bottom = max(np.argwhere(maskAB>0), key=lambda x: x[0])[0]
-	left = min(np.argwhere(imgB>0), key=lambda x: x[1])[1]
-	right = max(np.argwhere(imgB>0), key=lambda x: x[1])[1]
-	print(left, right, top, bottom)
+	imgA_gray = rgb2gray(imgA)
+	imgB_gray = rgb2gray(imgB)
+	# maskAB = get_overlapping_mask(imgA, imgB)
+	# top = min(np.argwhere(maskAB>0), key=lambda x: x[0])[0]
+	# bottom = max(np.argwhere(maskAB>0), key=lambda x: x[0])[0]
+	top = min(np.argwhere(imgB_gray>0), key=lambda x: x[0])[0]
+	bottom = max(np.argwhere(imgB_gray>0), key=lambda x: x[0])[0]
+	left = min(np.argwhere(imgB_gray>0), key=lambda x: x[1])[1]
+	right = max(np.argwhere(imgA_gray>0), key=lambda x: x[1])[1]
 
 	overlap_img = imgB[top:bottom, left:right]
 	e = genEngMap(overlap_img)
@@ -46,29 +51,35 @@ def seam_carving_blend_left(imgA, imgB):
 		idx = seam_idxs[i]
 		offset_y = left + idx
 		offset_x = top + i
-		blended_img[offset_x,offset_y:] = imgB[offset_x,offset_y:]
+		blended_img[offset_x,offset_y:,:] = imgB[offset_x,offset_y:,:]
 	
 	return blended_img
 
 def seam_carving_blend_right(imgB, imgC):
+	imgB_gray = rgb2gray(imgB)
+	imgC_gray = rgb2gray(imgC)
 	maskBC = get_overlapping_mask(imgB, imgC)
-	top = min(np.argwhere(maskBC>0), key=lambda x: x[0])[0]
-	bottom = max(np.argwhere(maskBC>0), key=lambda x: x[0])[0]
-	left = min(np.argwhere(imgC>0), key=lambda x: x[1])[1]
-	right = max(np.argwhere(imgB>0), key=lambda x: x[1])[1]
-	print(left, right, top, bottom)
+	# top = min(np.argwhere(maskBC>0), key=lambda x: x[0])[0]
+	# bottom = max(np.argwhere(maskBC>0), key=lambda x: x[0])[0]
+	top = min(np.argwhere(imgB_gray>0), key=lambda x: x[0])[0]
+	bottom = max(np.argwhere(imgB_gray>0), key=lambda x: x[0])[0]
+	left = min(np.argwhere(imgC_gray>0), key=lambda x: x[1])[1]
+	right = max(np.argwhere(imgB_gray>0), key=lambda x: x[1])[1]
 
 	overlap_img = imgB[top:bottom, left:right]
 	e = genEngMap(overlap_img)
 	Mx, Tbx = cumMinEngVer(e)
 	seam_idxs = getVerSeam(overlap_img, Mx, Tbx)
-	blended_img = np.copy(imgB) + imgC - np.multiply(maskBC,imgC)
+	blended_img = np.copy(imgB)
+	blended_img[:,:,0] = imgB[:,:,0] + imgC[:,:,0] - np.multiply(maskBC,imgC[:,:,0])
+	blended_img[:,:,1] = imgB[:,:,1] + imgC[:,:,1] - np.multiply(maskBC,imgC[:,:,1])
+	blended_img[:,:,2] = imgB[:,:,2] + imgC[:,:,2] - np.multiply(maskBC,imgC[:,:,2])
 
 	for i in range(len(seam_idxs)):
 		idx = seam_idxs[i]
 		offset_y = left + idx
 		offset_x = top + i
-		blended_img[offset_x,offset_y:] = imgC[offset_x,offset_y:]
+		blended_img[offset_x,offset_y:,:] = imgC[offset_x,offset_y:,:]
 
 	return blended_img
 
@@ -79,13 +90,13 @@ def seam_carving_blend_right(imgB, imgC):
 # imgA = np.array(Image.open('blend_left.jpg').convert('RGB'))
 # imgB = np.array(Image.open('blend_middle.jpg').convert('RGB'))
 # imgC = np.array(Image.open('blend_right.jpg').convert('RGB'))
-# imgA_gray = rgb2gray(imgA)
-# imgB_gray = rgb2gray(imgB)
-# imgC_gray = rgb2gray(imgC)
-# mask = get_overlapping_mask(imgA, imgB)
-# imgAB_blend = seam_carving_blend_left(imgA_gray, imgB_gray)
-# imgABC_blend = seam_carving_blend_right(imgAB_blend, imgC_gray)
-# # plt.imshow(imgAB_blend,cmap='gray')
-# # plt.show()
-# plt.imshow(imgABC_blend,cmap='gray')
+# # imgA_gray = rgb2gray(imgA)
+# # imgB_gray = rgb2gray(imgB)
+# # imgC_gray = rgb2gray(imgC)
+# # mask = get_overlapping_mask(imgA, imgB)
+# imgAB_blend = seam_carving_blend_left(imgA, imgB)
+# imgABC_blend = seam_carving_blend_right(imgAB_blend, imgC)
+# plt.imshow(imgAB_blend)
+# plt.show()
+# plt.imshow(imgABC_blend)
 # plt.show()
