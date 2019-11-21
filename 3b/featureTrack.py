bba4e71b5beb390a8ace605621e5ad97bd9a807d
JiatongSun <SJT_sjtu1998@163.com>
2019-11-21 11:57:37 -0500

    organize code

---
+++ 3b/featureTrack.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 00:28:25 2019
+
+@author: Jiatong Sun
+"""
+
+import numpy as np
+import scipy.signal as signal
+import cv2 as cv
+from helpers import rgb2gray
+from helpers import GaussianPDF_2D
+from helpers import flipChannel
+from helpers import generatePatch
+from helpers import interp2
+
+def estimateAllTranslation(startXs,startYs,img1,img2):
+    row,col = img1.shape[0], img1.shape[1];
+    N,F = startXs.shape[0], startXs.shape[1];
+    I_gray_1 = rgb2gray(img1);
+    G = GaussianPDF_2D(0,3,4,4);
+    [dx,dy] =  np.gradient(G, axis = (1,0));
+    Ix = signal.convolve2d(I_gray_1,dx,'same');
+    Iy = signal.convolve2d(I_gray_1,dy,'same');
+    newXs = np.zeros((N,F),dtype = np.int32);
+    newYs = np.zeros((N,F),dtype = np.int32);
+    for i in range(N):
+        for j in range(F):
+            newX, newY = estimateFeatureTranslation(startXs[i][j],\
+                                startYs[i][j], Ix, Iy, img1, img2)
+            if newX>=0 and newX<col and newY>=0 and newY<row>0:
+                newXs[i][j] = newX;
+                newYs[i][j] = newY;
+                
+    return newXs, newYs;
+
+def estimateFeatureTranslation(startX, startY, Ix, Iy, img1, img2):
+    I_gray_1 = rgb2gray(img1);
+    I_gray_2 = rgb2gray(img2);
+    X_old, Y_old = generatePatch(startX,startY);
+    X_old, Y_old = X_old.astype(np.int32),Y_old.astype(np.int32)
+    x0, y0 = startX, startY;
+    min_error = 999999;
+    newX, newY = 0, 0;
+    
+    iteration = 20;
+    for i in range(iteration):
+        X_new,Y_new = generatePatch(x0,y0);
+        old_coor = np.array((x0,y0)).reshape(-1,1);
+        It_temp = interp2(I_gray_2,X_new,Y_new) - I_gray_1[Y_old,X_old];
+        error = np.linalg.norm(It_temp);
+        if error < min_error:
+            min_error = error;
+            newX, newY = x0,y0;
+        
+        Ix_temp = Ix[Y_old,X_old];
+        Iy_temp = Iy[Y_old,X_old];
+        A = np.hstack((Ix_temp.reshape(-1,1),Iy_temp.reshape(-1,1)));
+        b = It_temp.reshape(-1,1);
+        flow_temp = np.linalg.solve(np.dot(A.T,A),-np.dot(A.T,b));
+        new_coor = old_coor + flow_temp;
+        x0, y0 = new_coor[0], new_coor[1];
+        
+    return newX, newY;
+
+def applyGeometricTransformation(startXs, startYs, newXs, newYs, bbox):
+    
+
+img1 = flipChannel(cv.imread('1.jpg'));
+img2 = flipChannel(cv.imread('17.jpg'));
+newXs, newYs = estimateAllTranslation(feat_x,feat_y,img1,img2);
--- 3b/getFeatures.py
+++ 3b/getFeatures.py
@@ -1,13 +1,7 @@
 import numpy as np
-import scipy.ndimage as ndimage
-import scipy.signal as signal
-from helpers import rgb2gray
-from PIL import Image
-import matplotlib.pyplot as plt
-from skimage import filters
 from skimage import feature
-
-from getFeatureForOne import getFeatureForOne
+from helpers import rgb2gray
+from helpers import flipChannel
 
 def getFeatures(img,bbox):
     num_box = bbox.shape[0];
@@ -17,21 +11,17 @@ def getFeatures(img,bbox):
     N = 0;
 
     for i in range((num_box)):
-      # h = bbox[0,2,1] - bbox[0,1,1]
-      # w = bbox[0,1,0] - bbox[0,0,0]
-      row_start = int(bbox[i,1,1])
-      row_end = int(bbox[i,2,1])
-      col_start = int(bbox[i,0,0])
-      col_end = int(bbox[i,1,0])
-      img_temp = img[row_start:row_end,col_start:col_end]
-      # plt.imshow(img_temp)
-      # plt.show()
-      x, y, rmax = getFeatureForOne(img_temp)
-      x += col_start
-      y += row_start
-      N = max(N, x.shape[0])   
-      res_x_temp[0,i] = x.reshape(-1)
-      res_y_temp[0,i] = y.reshape(-1)
+        row_start = int(bbox[i,1,1]);
+        row_end = int(bbox[i,2,1]);
+        col_start = int(bbox[i,0,0]);
+        col_end = int(bbox[i,1,0]);
+        img_temp = img[row_start:row_end,col_start:col_end];
+        x, y, rmax = getBoxFeature(img_temp);
+        x += col_start;
+        y += row_start;
+        N = max(N, x.shape[0]);
+        res_x_temp[0,i] = x.reshape(-1);
+        res_y_temp[0,i] = y.reshape(-1);
 
     res_x = np.zeros((N,num_box));
     res_y = np.zeros((N,num_box));
@@ -41,28 +31,40 @@ def getFeatures(img,bbox):
       res_x[0:size_temp,i] = res_x_temp[0,i];
       res_y[0:size_temp,i] = res_y_temp[0,i];
 
-    return res_x, res_y
+    return res_x, res_y;
 
-if __name__ == "__main__": 
-    # test
-    im = Image.open("1.jpg")
-    np_im = np.array(im)
-    img_gray = rgb2gray(np_im)
-    bbox = np.zeros((1,4,2))
-    #left top 
-    bbox[0,0,0] = 290
-    bbox[0,0,1] = 185
-    #right top 
-    bbox[0,1,0] = 400
-    bbox[0,1,1] = 185
-    #left bottom 
-    bbox[0,2,0] = 290
-    bbox[0,2,1] = 270
-    #right bottom 
-    bbox[0,3,0] = 400
-    bbox[0,3,1] = 270
+def getBoxFeature(img):
+    max_pts = 5;
     
-    res_x, res_y = getFeatures(img_gray, bbox)
-    plt.imshow(im)
-    plt.scatter(res_x,res_y)
-    plt.show()
+    if (img.ndim == 3):
+        img = rgb2gray(flipChannel(img));
+    cimg = feature.corner_harris(img);
+
+    cimg[0:5],cimg[-1:-6:-1],cimg[:,0:5],cimg[:,-1:-6:-1] = 0,0,0,0;
+    thresh = 0.012*cimg.max();
+    y_ind,x_ind = np.where(cimg>thresh);
+    val = cimg[cimg>thresh];
+    
+    if x_ind.size < max_pts:
+        x,y = x_ind,y_ind;
+        rmax = 999999;
+        return x,y,rmax
+    
+    radius = np.zeros((x_ind.size,1));
+    c = 0.9;
+    max_val = c * val.max();
+    for i in range(x_ind.size):
+        if val[i] > max_val:
+            radius[i] = 999999;
+            continue;
+        else:
+            dist = np.sqrt((x_ind - x_ind[i])**2 + (y_ind - y_ind[i])**2);
+            dist = dist[val * c > val[i]];
+            radius[i] = dist.min();
+    radius = radius.reshape(-1);
+    index = np.argsort(-radius);
+    index = index[0:max_pts];
+    
+    x,y = x_ind[index].reshape(-1,1),y_ind[index].reshape(-1,1);
+    rmax = radius[index[-1]];
+    return x, y, rmax;
