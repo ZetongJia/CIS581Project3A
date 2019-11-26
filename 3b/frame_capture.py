9e6e998b0b555c655fe40b8ebbaaf5b87349b1c0
JiatongSun <SJT_sjtu1998@163.com>
2019-11-26 13:03:32 -0500

    bug cannot be fixed

--- 3b/frame_capture.py
+++ 3b/frame_capture.py
@@ -18,77 +18,58 @@ from helpers import flipChannel
 if __name__ == "__main__": 
     cap = cv.VideoCapture("Easy.mp4");
     fourcc = cv.VideoWriter_fourcc(*'MJPG');
-    out = cv.VideoWriter('output.avi',fourcc, 10, (640,360));
+#    out = cv.VideoWriter('output.avi',fourcc, 10, (640,360));
     frame_cnt = 0;
     max_object = 1;
-    bbox = np.zeros((max_object,4,2),dtype = np.int32);
-    F = 0;
+    bbox = np.zeros((max_object,4,2),dtype = np.int32); 
+    trace_x = list();
+    trace_y = list();
     while True: 
         ret,frame = cap.read();
-        frame_cnt = frame_cnt + 1;
-        row,col = frame.shape[0],frame.shape[1];
-#        print(frame.shape);
-        if frame_cnt == 1:
-            cv.imwrite(str(frame_cnt)+".jpg",frame);
-            last_frame = frame;
-            while True:
-                x,y,w,h = np.int32(cv.selectROI("roi", frame, fromCenter=False));
-                cv.destroyAllWindows();
-                cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2);
-                bbox[F,:,:] = getBoxPoints(x,y,w,h);
-                F = F + 1;
-                if F >= max_object:
+        if ret == True:
+            frame_cnt = frame_cnt + 1;
+            row,col = frame.shape[0],frame.shape[1];
+            if frame_cnt == 1:
+                cv.imwrite(str(frame_cnt)+".jpg",frame);
+                last_frame = frame;
+                F = 0;
+                while True:
+                    x,y,w,h = np.int32(cv.selectROI("roi", frame, fromCenter=False));
                     cv.destroyAllWindows();
-                    break;
-            feat_x, feat_y = getFeatures(frame, bbox);
-            drawPoints(frame,feat_x,feat_y,(0,0,255));
-            cv.imshow("feature points", frame);
-            cv.waitKey(0);
-            cv.destroyAllWindows();
-#        elif frame_cnt % 5 == 1:
-        else:
-            newXs, newYs = estimateAllTranslation(feat_x,feat_y,\
-                            flipChannel(last_frame),flipChannel(frame));
-            feat_x,feat_y,bbox = applyGeometricTransformation(feat_x,\
-                                                feat_y,newXs,newYs,bbox);
-#            print("");
-#            print(feat_x);
-#            print(feat_y);
-            last_frame = frame; 
-#            inlier_ind =  (feat_x >= 0) * (feat_x < col) *\
-#                      (feat_y >= 0) * (feat_y < row);
-#            feat_x = feat_x * inlier_ind;
-#            feat_y = feat_y * inlier_ind;
-#            bbox_orth = np.zeros(bbox.shape,dtype = np.int32);
-#            for f in range(bbox.shape[0]):
-#                new_corners_temp = bbox[f,:,:];
-#                corner_1 = new_corners_temp[0,0:2].reshape(1,-1);
-#                corner_2 = new_corners_temp[1,0:2].reshape(1,-1);
-#                corner_3 = new_corners_temp[2,0:2].reshape(1,-1);
-#                corner_4 = new_corners_temp[3,0:2].reshape(1,-1);
-#                new_corners_temp = np.array([corner_1,corner_2,corner_3,\
-#                                             corner_4],dtype = np.float32);
-#                x,y,w,h = cv.boundingRect(new_corners_temp);
-#                bbox_orth[f,:,:] = getBoxPoints(x,y,w,h);
-#                cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2);
-#                drawPoints(frame,feat_x[:,f],feat_y[:,f],(0,0,255));
-#            print(bbox);
-            for f in range(bbox.shape[0]):
-                cv.rectangle(frame,(bbox[f,0,0],bbox[f,0,1]),
-                            (bbox[f,3,0],bbox[f,3,1]),(0,255,0),2);
-                drawPoints(frame,feat_x[:,f],feat_y[:,f],(0,0,255));
-#        else:
-#            for f in range(bbox.shape[0]): 
-#                cv.rectangle(frame,(bbox[f,0,0],bbox[f,0,1]),
-#                            (bbox[f,3,0],bbox[f,3,1]),(0,255,0),2);
-#                drawPoints(frame,feat_x[:,f],feat_y[:,f],(0,0,255));
-                                    
-        out.write(frame);
-        cv.imshow("capture",frame);
-        if cv.waitKey(30) & 0xff == ord('q'):
-            cv.destroyAllWindows();
-            break;
-        elif cv.waitKey(30) & 0xff == ord('s'):
-            cv.imwrite(str(frame_cnt)+".jpg",frame);
+                    cv.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2);
+                    bbox[F,:,:] = getBoxPoints(x,y,w,h);
+                    F = F + 1;
+                    if F >= max_object:
+                        cv.destroyAllWindows();
+                        break;
+                feat_x, feat_y = getFeatures(frame, bbox);
+                trace_x.append(feat_x);
+                trace_y.append(feat_y);
+                drawPoints(frame,feat_x,feat_y,(0,0,255));
+                cv.imshow("feature points", frame);
+                cv.waitKey(0);
+                cv.destroyAllWindows();
+            else:
+                newXs, newYs = estimateAllTranslation(feat_x,feat_y,\
+                                flipChannel(last_frame),flipChannel(frame));
+                feat_x,feat_y,bbox = applyGeometricTransformation(feat_x,\
+                                                    feat_y,newXs,newYs,bbox);
+                last_frame = frame; 
+                trace_x.append(feat_x);
+                trace_y.append(feat_y);
+                for f in range(bbox.shape[0]):
+                    cv.rectangle(frame,(bbox[f,0,0],bbox[f,0,1]),
+                                (bbox[f,3,0],bbox[f,3,1]),(0,255,0),2);
+                    drawPoints(frame,feat_x[:,f],feat_y[:,f],(0,0,255));
+                for k in range(len(trace_x)):
+                    for f in range(trace_x[k].shape[1]):
+                        drawPoints(frame,trace_x[k][:,f],trace_y[k][:,f],(0,0,255));
+    #        out.write(frame);
+            cv.imshow("capture",frame);
+            if cv.waitKey(30) & 0xff == ord('q'):
+                cv.destroyAllWindows();
+                break;
+            elif cv.waitKey(30) & 0xff == ord('s'):
+                cv.imwrite(str(frame_cnt)+".jpg",frame);
     cap.release();
-    out.release();
+#    out.release();
--- 3b/get_features.py
+++ 3b/get_features.py
@@ -4,8 +4,104 @@ from skimage import feature
 from helpers import rgb2gray
 from helpers import flipChannel
 
+# Method 1:
+# get feature points by processing bbox
+def getFeatures(img,bbox):
+    num_box = bbox.shape[0];
+    res_x_temp = np.zeros((1,num_box),dtype = np.object);
+    res_y_temp = np.zeros((1,num_box),dtype = np.object);
+
+    N = 0;
+
+    for i in range((num_box)):
+        row_start = int(bbox[i,1,1]);
+        row_end = int(bbox[i,2,1]);
+        col_start = int(bbox[i,0,0]);
+        col_end = int(bbox[i,1,0]);
+        img_temp = img[row_start:row_end,col_start:col_end];
+        x, y = getBoxFeature(img_temp);
+        x += col_start;
+        y += row_start;
+        N = max(N, x.shape[0]);
+        res_x_temp[0,i] = x.reshape(-1);
+        res_y_temp[0,i] = y.reshape(-1);
+
+    res_x = np.zeros((N,num_box));
+    res_y = np.zeros((N,num_box));
+
+    for i in range((num_box)):
+        size_temp = res_x_temp[0,i].shape[0];
+        res_x[0:size_temp,i] = res_x_temp[0,i];
+        res_y[0:size_temp,i] = res_y_temp[0,i];
+
+    return res_x, res_y;
+
+# get feature points by good feature to track
+def getBoxFeature(img):
+    max_pts = 20;
+    row, col = img.shape[0], img.shape[1];
+    
+    if (img.ndim == 3):
+        img = rgb2gray(flipChannel(img));
+    corners = cv.goodFeaturesToTrack(img.astype(np.float32),max_pts,0.01,10);
+    corners = np.int0(corners);
+    x, y = corners[:,:,0], corners[:,:,1];
+    inlier_ind = (x>5) * (x<col-5) * (y>5) * (y<row-5);
+    x, y = x[inlier_ind], y[inlier_ind];
+    
+    return x, y;
+
+
+
+
 # =============================================================================
-# # get feature points by processing the whole image
+# # Method 2
+# # get feature points by harris
+# 
+# def getBoxFeature(img):
+#     max_pts = 25;
+#     
+#     if (img.ndim == 3):
+#         img = rgb2gray(flipChannel(img));
+#     cimg = feature.corner_harris(img);
+# 
+#     cimg[0:5],cimg[-1:-6:-1],cimg[:,0:5],cimg[:,-1:-6:-1] = 0,0,0,0;
+#     thresh = 0.012*cimg.max();
+#     y_ind,x_ind = np.where(cimg>thresh);
+#     val = cimg[cimg>thresh];
+#     
+#     if x_ind.size < max_pts:
+#         x,y = x_ind,y_ind;
+#         rmax = 999999;
+#         return x,y,rmax
+#     
+#     radius = np.zeros((x_ind.size,1));
+#     c = 0.9;
+#     max_val = c * val.max();
+#     for i in range(x_ind.size):
+#         if val[i] > max_val:
+#             radius[i] = 999999;
+#             continue;
+#         else:
+#             dist = np.sqrt((x_ind - x_ind[i])**2 + (y_ind - y_ind[i])**2);
+#             dist = dist[val * c > val[i]];
+#             radius[i] = dist.min();
+#     radius = radius.reshape(-1);
+#     index = np.argsort(-radius);
+#     index = index[0:max_pts];
+#     
+#     x,y = x_ind[index].reshape(-1,1),y_ind[index].reshape(-1,1);
+#     return x, y;
+# =============================================================================
+
+
+
+
+
+# =============================================================================
+# # Method 3
+# # get feature points by processing the whole image and harris corner
+# 
 # def getFeatures(img,bbox):
 #     max_pts = 30;
 #     
@@ -63,90 +159,3 @@ from helpers import flipChannel
 #                     break;
 #     return res_x, res_y;
 # =============================================================================
-
-
-# get feature points by processing bbox
-def getFeatures(img,bbox):
-    num_box = bbox.shape[0];
-    res_x_temp = np.zeros((1,num_box),dtype = np.object);
-    res_y_temp = np.zeros((1,num_box),dtype = np.object);
-
-    N = 0;
-
-    for i in range((num_box)):
-        row_start = int(bbox[i,1,1]);
-        row_end = int(bbox[i,2,1]);
-        col_start = int(bbox[i,0,0]);
-        col_end = int(bbox[i,1,0]);
-        img_temp = img[row_start:row_end,col_start:col_end];
-#        x, y, rmax = getBoxFeature(img_temp);
-        x, y = getBoxFeature(img_temp);
-        x += col_start;
-        y += row_start;
-        N = max(N, x.shape[0]);
-        res_x_temp[0,i] = x.reshape(-1);
-        res_y_temp[0,i] = y.reshape(-1);
-
-    res_x = np.zeros((N,num_box));
-    res_y = np.zeros((N,num_box));
-
-    for i in range((num_box)):
-        size_temp = res_x_temp[0,i].shape[0];
-        res_x[0:size_temp,i] = res_x_temp[0,i];
-        res_y[0:size_temp,i] = res_y_temp[0,i];
-
-    return res_x, res_y;
-
-
-# =============================================================================
-# # get feature points by harris
-# def getBoxFeature(img):
-#     max_pts = 25;
-#     
-#     if (img.ndim == 3):
-#         img = rgb2gray(flipChannel(img));
-#     cimg = feature.corner_harris(img);
-# 
-#     cimg[0:5],cimg[-1:-6:-1],cimg[:,0:5],cimg[:,-1:-6:-1] = 0,0,0,0;
-#     thresh = 0.012*cimg.max();
-#     y_ind,x_ind = np.where(cimg>thresh);
-#     val = cimg[cimg>thresh];
-#     
-#     if x_ind.size < max_pts:
-#         x,y = x_ind,y_ind;
-#         rmax = 999999;
-#         return x,y,rmax
-#     
-#     radius = np.zeros((x_ind.size,1));
-#     c = 0.9;
-#     max_val = c * val.max();
-#     for i in range(x_ind.size):
-#         if val[i] > max_val:
-#             radius[i] = 999999;
-#             continue;
-#         else:
-#             dist = np.sqrt((x_ind - x_ind[i])**2 + (y_ind - y_ind[i])**2);
-#             dist = dist[val * c > val[i]];
-#             radius[i] = dist.min();
-#     radius = radius.reshape(-1);
-#     index = np.argsort(-radius);
-#     index = index[0:max_pts];
-#     
-#     x,y = x_ind[index].reshape(-1,1),y_ind[index].reshape(-1,1);
-#     rmax = radius[index[-1]];
-#     return x, y, rmax;
-# =============================================================================
-
-
-
-# get feature points by good feature
-def getBoxFeature(img):
-    max_pts = 25;
-    
-    if (img.ndim == 3):
-        img = rgb2gray(flipChannel(img));
-    corners = cv.goodFeaturesToTrack(img.astype(np.float32),max_pts,0.01,10);
-    corners = np.int0(corners);
-    x, y = corners[:,:,0], corners[:,:,1];
-    
-    return x, y;
